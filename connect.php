ab25a67f70a2af90790d1ac71c45d1a1d7f64cfb
Rong <rzheng@fau.edu>
2019-04-07 04:35:35 -0400

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+define('db_servername', 'localhost');
+define('db_username', 'rzheng');
+define('db_dbname', 'rzheng');
+define('db_pass', 'fjdskfjs');
+
+// Create connection
+$conn = new mysqli(db_servername, db_username, db_pass, db_dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
