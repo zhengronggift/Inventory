642ffb8e77dcd4917d3ac9bac7c527280d88cfbf
Rong <rzheng@fau.edu>
2019-04-11 17:13:07 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -9,17 +9,132 @@
        <title>Dealership Inventory</title>
        <link rel="stylesheet" href="main.css">
        <link rel="stylesheet" href="style.css">
-   </head>
 
+<style>
+div.gallery {
+  border: 3px solid #0f8eeaf7;
+
+}
+
+div.gallery:hover {
+  border: 3px solid #0f8eeaf7;
+}
+
+div.gallery img {
+  width: 100%;
+  height: auto;
+}
+
+div.desc {
+  padding: 15px;
+  text-align: center;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+.responsive {
+  padding: 0 6px;
+  float: center;
+  width: 24.99999%;
+}
+
+@media only screen and (max-width: 700px) {
+  .responsive {
+    width: 49.99999%;
+    margin: 6px 0;
+  }
+}
+
+@media only screen and (max-width: 500px) {
+  .responsive {
+    width: 100%;
+  }
+}
+
+.clearfix:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+#left
+{
+  float: left;
+}
+
+#center
+{
+  float: center;
+}
+
+#right
+{
+  float: right;
+}
+
+</style>
+
+
+</head>
+ 
 <body>
        <ul>
-           <li style="float:left"><a><img src="Rice&Beans_Logo.PNG" alt="rice&beans" width="20" height="20"> Welcome to DMS</a></li>
+           <li style="float:left"><a><img src="Rice&Beans_Logo.PNG" alt="circle" width="20" height="20"> Welcome to DMS</a></li>
             <li><a href="get_car.php?did=0">Vehicles</a></li>
            <li><a href= "get_dealer.php">Dealerships</a></li>
        </ul>
-       <br>   
-    <div id = "content" class="container">
-        <p>hello</p>
-    </div>
+       <br>
+       <p> </p>
+       <center>    
+
+<h3>Dealership Management System</h3>
+<br><br><br>
+<div class="responsive">
+  <div class="gallery">
+<h4>By:</h4>
+<h4>Szelai Choi</h4>
+<h4>Cesar Jaimes</h4>
+<h4>Chantel Ma</h4>
+<h4>Rong Zheng</h4>
+<br>
+</div>
+</div>
+<br>
+<div class="w3-content w3-section" style="max-width:500px">
+<?php
+include('connect.php');
+$db = mysqli_connect(db_servername, db_username, db_pass, db_dbname);
+		//echo $file;
+		
+		$sql = "SELECT * FROM vehicle";
+		$result = mysqli_query($db, $sql);
+		while($row = mysqli_fetch_assoc($result)){
+		$vin = $row['vin_num'];
+		$picture = $vin . ".jpg";
+		echo "<img class='mySlides w3-animate-fading' src='picture/". $picture . "' style='width:90%'>";
+		}
+
+?>
+
+</div>
+<script>
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 5000);    
+}
+</script>
+
 </body>
 </html>
