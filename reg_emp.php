2960e0add7819d90eb57f0c3c6e2286e4ad12a68
Rong <rzheng@fau.edu>
2019-04-10 21:37:49 -0400

    Add files via upload

--- reg_emp.php
+++ reg_emp.php
@@ -1,3 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+       <meta charset="utf-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1">
+       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+       <title>Dealership Inventory</title>
+       <link rel="stylesheet" href="main.css">
+       <link rel="stylesheet" href="style.css">
+</head>
+ 
+<body>
+       <ul>
+           <li style="float:left"><a><img src="Rice&Beans_Logo.PNG" alt="circle" width="20" height="20"> Welcome to DMS</a></li>
+            <li><a href="get_car.php?did=0">Vehicles</a></li>
+           <li><a href= "get_dealer.php">Dealerships</a></li>
+       </ul>
+       <br>
+       <p> </p>
+       <center> 
+
+<?php
+//to get dealer info
+include('connect.php');
+		$db = mysqli_connect(db_servername, db_username, db_pass, db_dbname);
+		//echo $file;
+		$did = $_GET['did'];
+		$sql = "SELECT * FROM dealer WHERE d_id = '$did'";
+		$sql2 = "SELECT * FROM manage m, employee e WHERE m.d_id = '$did' AND e.e_id = m.e_id";
+		$result = mysqli_query($db, $sql);
+		$row = mysqli_fetch_assoc($result);
+		$result2 = mysqli_query($db, $sql2);
+		$row2 = mysqli_fetch_assoc($result2);
+		$city = $row['city'];
+		$state = $row['state'];
+		$zip = $row['zip_code'];
+		$name = $row['name'];
+		$m_f = $row2['f_name'];
+		$m_l = $row2['l_name'];
+	
+        	echo $name . " (Dealer ID:" . $did . ")<br>";
+		echo $city . ", ";
+        	echo $state . ", ";
+        	echo $zip . "<br> ";
+        	echo "Store Manager: " . $m_f . " " . $m_l . "<br>";
+?>
+
 <p>Register</p>
 <form action="emp_back.php?did=<?php echo $_GET['did']; ?>" method="POST">
 <p>Dealer ID: <?echo $_GET['did']; ?></p>
@@ -5,8 +54,10 @@
 <p>Last Name: <input type="text" name="l_name" size="15" maxlength="20" value = ""></p>
 <p>Role: <input type="radio" name="emp_man" value="employee" checked> Employee   
 <input type="radio" name="emp_man" value="manager"> Manager</p>
-<p><input type="submit" class="button button1" name="register" value="Register"></p>
+<p><button class="button button1" type="submit" name="register" value="Register">Register</button>
 </form>
 
-	
+</div>
+</body>
+</html>
 
